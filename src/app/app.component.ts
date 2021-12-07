ca3ac10fd97c0d2a9c1a93fd32c19aca809c2f9b
Christopher (Bonus) Benzinger <bonus@benzinger-it.com>
2021-12-07 19:00:23 +0100

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'testing';
+
+  constructor(
+    private _snackBar: MatSnackBar,
+  ) {}
+
+  isLoading = false;
+
+  form = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    mail: new FormControl('', [Validators.required, Validators.email]),
+    content: new FormControl('',),
+    privacy: new FormControl('', Validators.requiredTrue)
+  });
+
+
+  cancel() {
+    this.form.setValue({
+      name: '',
+      mail: '',
+      content: '',
+      privacy: false
+    })
+  }
+
+  send() {
+    this.isLoading = true;
+    setTimeout(() => {
+      this.cancel();
+      this.isLoading = false;
+      this._snackBar.open('Formular erfolgreich versendet!', 'Ok.', {
+        panelClass: 'success',
+        duration: 3000,
+      })
+    }, 3000);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
   declarations: [
@@ -10,7 +19,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatProgressSpinnerModule,
+    MatCheckboxModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatSnackBarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
